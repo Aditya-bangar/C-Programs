752f5d34b7f7027ddec7fa4d5f48b4b7e96f2be9
Aditya-bangar <adibangar97@gmail.com>
2024-08-21 22:28:32 +0530

    even odd position  in array

---
+++ evenOddPattern.c
@@ -0,0 +1,19 @@
+
+// Given and array of intger change the value of all odd index element to its second multiple and increment all even 
+// indexed value bye 10
+
+#include<stdio.h>
+int main()
+{
+    int arr[7]={1,2,3,4,5,6,7};
+    for(int i=0;i<=6;i++)
+    {
+        if(i%2!=0) arr[i]*=2;
+        else arr[i]+=10;
+        }
+        for(int i=0;i<=6;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+   return 0;
+}
