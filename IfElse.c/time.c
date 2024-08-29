4030f3789922f3082e95e8ba1cea540e49a052ec
Aditya-bangar <adibangar97@gmail.com>
2024-08-29 08:22:02 +0530

    time

---
+++ IfElse.c/time.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int time , minute;
+
+    puts("Enter the time in 24 hour and minute formate \n");
+    scanf("%d %d",&time,&minute);
+
+    if(time<=12){
+        printf("The Time is %d:%d  AM",time,minute);
+    }else{
+        printf("The Time is %d:%d  PM",time,minute);
+    }
+    return 0;
+}
